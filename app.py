c81afac4f2912f6f064f784f233ec589e18d7764
git status <215340863+g-sts@users.noreply.github.com>
2025-08-07 05:16:41 -0300

    feat: cria rotas GET e POST para produtos com modelo Pydantic

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -18,4 +21,36 @@ PRODUTOS = [
         "disponivel": False
     }
 ]
+
+class Produto(BaseModel):
+    """Classe de produto."""
+    
+    nome: str
+    descricao: Optional[str] = None
+    preco: float
+    disponivel: Optional[bool] = True
+
+
+@app.get("/produtos", tags = ["produtos"])
+def listar_produtos() -> list:
+    """Listar produtos."""
+    return PRODUTOS
+
+@app.get("/produtos/{produto_id}", tags = ["produtos"])
+def obter_produto(produto_id: int) -> dict:
+    """Obter produto."""
+    for produto in PRODUTOS:
+        if produto["id"] == produto_id:
+            return produto
+    return {}
+
+@app.post("/produtos", tags = ["produtos"])
+def criar_produto(produto: Produto) -> dict:
+    """Criar produto."""
+    produto = produto.dict()
+    produto["id"] = len(PRODUTOS) + 1
+    PRODUTOS.append(produto)
+    return produto
+
+
 
